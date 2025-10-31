a496cd25e6d7b94fae84421d748a8aa658395d40
Abdullah <abdullahmug2332@gmail.com>
2025-10-31 10:47:05 -0700

    add about

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,10 +6,10 @@ import { motion } from "framer-motion";
 
 export default function Contact() {
   return (
-    <div className='container  pad mar !mb-0 flex flex-col justify-center items-center bg-[var(--accordion)] rounded-t-[50px]'>
+    <div className='container  pad mar !mb-0  flex flex-col justify-center items-center bg-[var(--accordion)] rounded-t-[50px]'>
       <button className='flex items-center gap-[10px]  px-[20px] py-[7px] rounded-full font-[600]'><span className='color text-[35px]'>•</span><SplitText className='para font-[600]' text="Available for Work" /></button>
 
-      <p className='font-[700] lfont font text-[40px] md:text-[60px] lg:text-[62px] xl:text-[70px] 2xl:text-[80px] leading-[40px] md:leading-[70px] xl:leading-[70px] 2xl:leading-[80px] w-[80%] xl:w-[50%] text-center mt-[20px]'>Let's create your next big idea</p>
+      <p className='font-[700] lfont font text-[40px] py-[20px] sm:py-0 md:text-[60px] lg:text-[62px] xl:text-[70px] 2xl:text-[80px] leading-[40px] md:leading-[70px] xl:leading-[70px] 2xl:leading-[80px] w-[80%] xl:w-[50%] text-center mt-[20px]'>Let's create your next big idea</p>
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
