34388b74a875c3365660f3e09a8f7a1d219813a9
Abdullah <abdullahmug2332@gmail.com>
2025-11-12 11:24:59 -0800

    add des in contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,6 +10,8 @@ export default function Contact() {
       <button className='flex items-center gap-[10px]  px-[20px] py-[7px] rounded-full font-[600]'><span className='color text-[35px]'>•</span><SplitText className='para font-[600]' text="Available for Work" /></button>
 
       <p className='font-[700] lfont font text-[40px] py-[20px] sm:py-0 md:text-[60px] lg:text-[62px] xl:text-[70px] 2xl:text-[80px] leading-[40px] md:leading-[70px] xl:leading-[70px] 2xl:leading-[80px] w-[80%] xl:w-[50%] text-center mt-[20px]'>Let's create your next big idea</p>
+      <p className='text-center mt-[20p] md:mt-[40px] font-[600] w-[95%] md:w-[80%] lg:w-[70%] xl:w-[60%] ' >I'm always excited to collaborate on innovative web projects, whether it's crafting pixel-perfect front-end experiences, building scalable back-end systems, or bringing bold ideas to life with modern frameworks. 
+    Let's create something extraordinary together!</p>
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
