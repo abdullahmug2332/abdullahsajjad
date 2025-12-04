2e6d199847182604497389b434dd6f0d824df030
Abdullah <abdullahmug2332@gmail.com>
2025-12-05 00:35:15 +0500

    add unoptimized to blog image

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,13 +1,12 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
-import SplitText from "@/components/SplitText";
 import { motion } from "framer-motion";
 
 export default function Contact() {
   return (
     <div className='container  pad mar !mb-0  flex flex-col justify-center items-center bg-[var(--accordion)] rounded-t-[50px]'>
-      <button className='flex items-center gap-[10px]  px-[20px] py-[7px] rounded-full font-[600]'><span className='color text-[35px]'>•</span><SplitText className='para font-[600]' text="Available for Work" /></button>
+      <button className='flex items-center gap-[10px]  px-[20px] py-[7px] rounded-full font-[600]'><span className='color text-[35px]'>•</span><p className='para font-[600]' >Available for Work</p></button>
 
       <p className='font-[700] lfont font text-[40px] py-[20px] sm:py-0 md:text-[60px] lg:text-[62px] xl:text-[70px] 2xl:text-[80px] leading-[40px] md:leading-[70px] xl:leading-[70px] 2xl:leading-[80px] w-[80%] xl:w-[50%] text-center mt-[20px]'>Let's create your next big idea</p>
       <p className='text-center mt-[20p] md:mt-[40px] font-[600] w-[95%] md:w-[80%] lg:w-[70%] xl:w-[60%] ' >I'm always excited to collaborate on innovative web projects, whether it's crafting pixel-perfect front-end experiences, building scalable back-end systems, or bringing bold ideas to life with modern frameworks. 
