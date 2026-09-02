4ac47b165acdefb4db7bb522cd58c5bebdf662e3
Abdullah <abdullahmug2332@gmail.com>
2026-09-02 19:57:04 +0500

    chnage img

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,8 @@ export default function Home() {
   const home = {
     hero: {
       image: {
-        src: "/abdullah2.jpg",
+        src: "/abdullah.JPG",
+        // src: "/abdullah2.jpg",
         alt: "abdullah",
         cta: {
           text: "LETS TALK * LETS TALK * LETS TALK * LETS TALK * ",
